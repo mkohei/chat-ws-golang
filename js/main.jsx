a7d62b1ffe5c325bd82cbb23d1e76d5030664146
mkohei <baberugaberiimeronnv5@gmail.com>
2018-08-22 20:34:17 +0900

    骨格できた

--- js/main.jsx
+++ js/main.jsx
@@ -1,5 +1,42 @@
+var myName = "NONAME";
+
 var wsUri = "ws://localhost:8080/echo";
 var websocket = null;
+
+let data = {};
+class App extends React.Component {
+    constructor(props, context, updater) {
+        super(props, context, updater)
+        this.state = { list : [] }
+        this.addToList = this.addToList.bind(this)
+    }
+    componentDidMount() {
+        this.props.data.App = this;
+    }
+    componentWillUnmount() {
+        delete this.props.data.App;
+    }
+    addToList(message) {
+        this.state.list.unshift(message)
+        this.setState({ list : this.state.list }) // re-lender
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.list.map((message) => {
+                        return <li>
+                            <div>NAME : {message.name}</div>
+                            <div>MESSAGE : {message.content}</div>
+                        </li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+
 
 function init() {
     websocket = new WebSocket(wsUri);
@@ -10,21 +47,20 @@ function init() {
 }
 
 function onOpen(event) {
-    console.log(event);
-    console.log("Connected");
 }
 
 function onClose(event) {
-    console.log(event);
 }
 
 function onMessage(evenet) {
-    const data = event.data;
-    console.log(data);
+    const d = event.data;
+    const message = JSON.parse(d);
+    console.log(message)
+    console.log(data)
+    data.App.addToList(message)
 }
 
 function onError(event) {
-    console.log(event);
 }
 
 
@@ -34,16 +70,28 @@ function onError(event) {
 
 document.getElementById("connect-button").onclick = function() {
     init();
+    const nametext = document.getElementById("name-text")
+    var val = nametext.value;
+    if (val == null || val.length == 0);
+    else {
+        myName = val;
+    }
+    nametext.value = myName;
+    nametext.disabled = true;
+    
     this.innerText = "Connected";
     this.disabled = true;
 }
 
 document.getElementById("send-button").onclick = function() {
+    //console.log("button: ", websocket)
+
     if (websocket == null) return;
     var val = document.getElementById("input-text").value;
     if (val == null || val.length == 0);
     else {
         var json = JSON.stringify({
+            name : myName,
             content : val
         });
         websocket.send(json);
@@ -55,13 +103,9 @@ document.getElementById("send-button").onclick = function() {
  * 
  */
 
-class Foo extends React.Component {
-    render() {
-        return <div>AAAA</div>;
-    }
-}
 
 ReactDOM.render(
-    <Foo />,
+    <App data={data}/>,
     document.getElementById("board")
 );
+
--- server.go
+++ server.go
@@ -11,21 +11,57 @@ type Data struct {
 	Content string
 }
 
-var ws_array []*websocket.Conn // *websocket.Connを入れる配列
+var (
+	ws_array []*websocket.Conn // *websocket.Connを入れる配列
+	conns    = map[*websocket.Conn]bool{}
+)
 
 func echoHandler(ws *websocket.Conn) {
+	/*
+		ws_array = append(ws_array, ws)
+		conns[ws] = true
+
+		var d Data
+		websocket.JSON.Receive(ws, &d)
+
+		for _, con := range ws_array {
+			fmt.Println("value:", con)
+			websocket.JSON.Send(con, d)
+		}
+
+		fmt.Printf("****\n%#v\n", d)
+		fmt.Printf("****\n%#v\n", ws_array)
+		//fmt.Printf("****\n%#v\n", conns)
+		//io.Copy(ws, ws)
+	*/
 	ws_array = append(ws_array, ws)
+	//conns[ws] = true
 
-	var d Data
-	websocket.JSON.Receive(ws, &d)
+	var err error
+	for {
+		var content string
 
-	for _, con := range ws_array {
-		fmt.Println("value:", con)
-		websocket.JSON.Send(con, d)
+		if err = websocket.Message.Receive(ws, &content); err != nil {
+			fmt.Println("Cant receive")
+			break
+		}
+
+		fmt.Println("Received back from client: " + content)
+
+		msg := "Received: " + content
+		fmt.Println("Sensing to client: " + msg)
+
+		for _, con := range ws_array {
+			fmt.Println("value:", con)
+			websocket.Message.Send(con, content)
+		}
+		/*
+			if err = websocket.Message.Send(ws, msg); err != nil {
+				fmt.Println("Cant send")
+				break
+			}
+		*/
 	}
-
-	fmt.Printf("%#v\n", d)
-	//io.Copy(ws, ws)
 }
 
 func main() {
